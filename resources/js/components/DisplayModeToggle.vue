9b1325e7d4c04c3179b1f237edfa314cac1b582d
Marjose Darang <18107626+MarJose123@users.noreply.github.com>
2026-04-03 16:02:47 +0800

    feat(http): integrate Inertia's http client
    - Remove explicit Axios dependency and usage.
    - Integrate Inertia's built-in http client for network requests.
    - Centralise global HTTP error handling for session expiry.
    - Update composables and components to use the new http client.
    - Introduce a Bun configuration file to manage installations.

--- resources/js/components/DisplayModeToggle.vue
+++ resources/js/components/DisplayModeToggle.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { useHttp } from "@inertiajs/vue3";
 import { SunMoon } from "lucide-vue-next";
 import { Button } from "@/components/ui/button";
 import {
@@ -9,15 +10,17 @@ import {
 } from "@/components/ui/tooltip";
 import type { Appearance } from "@/composables/useAppearance";
 import { useAppearance } from "@/composables/useAppearance";
-import axiosClient from "@/lib/axios";
 
 const { appearance, updateAppearance } = useAppearance();
 
 const toggleAppearance = (appearance: Appearance) => {
+    const http = useHttp({
+        mode: "",
+    });
     updateAppearance(appearance);
+    http.mode = appearance;
     // persist user appearance into the system database
-    axiosClient
-        .patch(route("appearance.update"), { mode: appearance })
+    http.patch(route("appearance.update"))
         .then()
         .catch(() => {
             console.error(
